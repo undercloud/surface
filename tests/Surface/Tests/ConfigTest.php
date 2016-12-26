af55379a57d0e98b0f2036e3d076f0594fc427eb
mooden <alexandrostruses@yandex.ru>
2016-12-26 08:01:45 +0400

    dump

--- tests/Surface/Tests/ConfigTest.php
+++ tests/Surface/Tests/ConfigTest.php
@@ -6,15 +6,29 @@ use PHPUnit_Framework_TestCase;
 
 class ConfigTest extends PHPUnit_Framework_TestCase
 {
-    private $config;
-
     public function __construct()
     {
-        $this->config = (new Surface)->config();
+        $config = (new Surface)->config();
     }
 
-    public function testReadAll()
+    public function testConfigStack()
     {
+        $config = (new Surface)->config();
 
+        $all = $config->all();
+        $from = $config->from('session');
+        $config->set('session.cookie_path', '/go');
+        $config->restore('session.cookie_path');
+
+        $memory = $config->get('post_max_size', 'bytes');
+        $errors = $config->get('display_errors', 'bool');
+    }
+
+    public function testInclude()
+    {
+        $config = (new Surface)->config();
+
+        $included = $config->included();
+        $source = $config->source();
     }
 }
